1448aa5f04b9344a3d6de5802b6342bd22a59f31
Vincent Boulos <vboulos@redhat.com>
2023-11-06 14:09:24 -0500

    add AferEach fucntion to teardown the secret

--- e2e-go/pkg/test/hcp_rhacm4k-21843_test.go
+++ e2e-go/pkg/test/hcp_rhacm4k-21843_test.go
@@ -25,6 +25,18 @@ var _ = ginkgo.Describe("RHACM4K-21843: Hypershift: Hypershift Addon should dete
 		podNameAfter     string
 	)
 
+	ginkgo.AfterEach(func() {
+		secret, err := utils.GetSecretInNamespace(kubeClient, namespace, secretName)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Failed to get secret")
+		gomega.Expect(secret).NotTo(gomega.BeNil(), "Secret not found")
+		gomega.Expect(secret.Data).NotTo(gomega.BeEmpty(), "Secret data is empty")
+
+		// Remove the new key-value pair we've added in Step 3
+		delete(secret.Data, newKey)
+		_, err = kubeClient.CoreV1().Secrets(namespace).Update(context.TODO(), secret, metav1.UpdateOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	})
+
 	ginkgo.It("Get, modify, and verify the s3 secret", func() {
 		ginkgo.By("Step 1: Get the latest hypershift install Pod BEFORE updating the secret", func() {
 			podBefore, err := utils.GetLatestCreatedPod(kubeClient, namespace2)
@@ -65,16 +77,16 @@ var _ = ginkgo.Describe("RHACM4K-21843: Hypershift: Hypershift Addon should dete
 			fmt.Printf(" %s != %s \n", podNameAfter, podNameBefore)
 			gomega.Ω(podNameAfter).ShouldNot(gomega.Equal(podNameBefore))
 		})
-		ginkgo.By("Step 5: Rollback the secret to its original value)", func() {
-			secret, err := utils.GetSecretInNamespace(kubeClient, namespace, secretName)
-			gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Failed to get secret")
-			gomega.Expect(secret).NotTo(gomega.BeNil(), "Secret not found")
-			gomega.Expect(secret.Data).NotTo(gomega.BeEmpty(), "Secret data is empty")
+		// ginkgo.By("Step 5: Rollback the secret to its original value)", func() {
+		// 	secret, err := utils.GetSecretInNamespace(kubeClient, namespace, secretName)
+		// 	gomega.Expect(err).NotTo(gomega.HaveOccurred(), "Failed to get secret")
+		// 	gomega.Expect(secret).NotTo(gomega.BeNil(), "Secret not found")
+		// 	gomega.Expect(secret.Data).NotTo(gomega.BeEmpty(), "Secret data is empty")
 
-			// Remove the new key-value pair we've added in Step 3
-			delete(secret.Data, newKey)
-			_, err = kubeClient.CoreV1().Secrets(namespace).Update(context.TODO(), secret, metav1.UpdateOptions{})
-			gomega.Expect(err).NotTo(gomega.HaveOccurred())
-		})
+		// 	// Remove the new key-value pair we've added in Step 3
+		// 	delete(secret.Data, newKey)
+		// 	_, err = kubeClient.CoreV1().Secrets(namespace).Update(context.TODO(), secret, metav1.UpdateOptions{})
+		// 	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		// })
 	})
 })
